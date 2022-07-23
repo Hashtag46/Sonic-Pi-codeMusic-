9489cfe22749b498c6022059e59e87b7b3452751
Hashtag46 <89920792+Hashtag46@users.noreply.github.com>
2022-07-23 21:24:23 +0530

    a proper one :D
    note the two special sleep times, try to develop an intuition accordingly

---
+++ yankeedoodle.rb
@@ -0,0 +1,120 @@
+use_bpm 80
+use_synth :piano
+play :c4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+play :d4, amp: 3
+sleep 0.5
+play :e4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+play :e4, amp: 3
+sleep 0.5
+play :d4, amp: 3
+sleep 0.5
+play :g3, amp: 3
+sleep 0.5
+
+play :c4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+play :d4, amp: 3
+sleep 0.5
+play :e4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 1
+play :b3, amp: 3
+sleep 0.5
+play :g3, amp: 3
+sleep 0.5
+
+play :c4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+play :d4, amp: 3
+sleep 0.5
+play :e4, amp: 3
+sleep 0.5
+play :f4, amp: 3
+sleep 0.5
+play :e4, amp: 3
+sleep 0.5
+play :d4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+play :b3, amp: 3
+sleep 0.5
+play :g3, amp: 3
+sleep 0.5
+play :a3, amp: 3
+sleep 0.5
+play :b3, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 1
+play :c4, amp: 3
+sleep 1
+
+play :a3, amp: 3    #notice the sleep times, try to develop a good sense of musical timing, it's hard, true
+sleep 0.75
+play :b3, amp: 3
+sleep 0.25
+play :a3, amp: 3
+sleep 0.5
+play :g3, amp: 3
+sleep 0.5
+play :a3, amp: 3
+sleep 0.5
+play :b3, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 1
+
+play :g3, amp: 3    #here also the two sleep times
+sleep 0.75
+play :a3, amp: 3
+sleep 0.25
+play :g3, amp: 3
+sleep 0.5
+play :f3, amp: 3
+sleep 0.5
+play :e3, amp: 3
+sleep 1
+play :g3, amp: 3
+sleep 1
+
+play :a3, amp: 3
+sleep 0.75
+play :b3, amp: 3
+sleep 0.25
+play :a3, amp: 3
+sleep 0.5
+play :g3, amp: 3
+sleep 0.5
+play :a3, amp: 3
+sleep 0.5
+play :b3, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+
+play :a3, amp: 3
+sleep 0.5
+play :g3, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 0.5
+play :b3, amp: 3
+sleep 0.5
+play :d4, amp: 3
+sleep 0.5
+play :c4, amp: 3
+sleep 1
+play :c4, amp: 3
+sleep 1
